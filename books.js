f355ab0e47d3c7b9420ee7108c046c995c644257
Ye Wint <yeirwintaung0@gmail.com>
2023-11-02 13:39:17 +0630

    added reactjs practice
    subscription

---
+++ books.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+        id: 1,
+        img:'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL600_SR600,400_.jpg',
+        title:'I Love You to the Moon and Back',
+        author:'Amelia Hepworth',
+    },
+    {
+        id: 2,
+        img:'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg',
+        title:'Our class is a family',
+        author:'Shannon Olsen',
+    },
+    {
+        id: 3,
+        img:'https://m.media-amazon.com/images/I/713QrA632AL._AC_UY327_FMwebp_QL65_.jpg',
+        title:'The Vanishing Half: A GMA Book Club Pick (A Novel)',
+        author:'Brit Bennett',
+    }
+]
